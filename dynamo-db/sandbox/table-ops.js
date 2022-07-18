a4c057f5f89e60ec89624de10eb3aaa5eabd95d0
Douglas Souza <douglas.barbosa@sensedia.com>
2022-07-18 16:53:10 -0300

    Listing and Describing Tables using AWS SDK.

---
+++ dynamo-db/sandbox/table-ops.js
@@ -0,0 +1,22 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+
+const dynamodb = new AWS.DynamoDB();
+
+dynamodb.listTables({}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+
+dynamodb.describeTable({
+    TableName: "td_notes"
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(JSON.stringify(data, null, 2));
+    }
+});
